575f5b0f2d374b806df53c575ad47c2651315451
Martijn Verburg <martijnverburg@gmail.com>
2014-08-28 18:50:24 +0100

    Add The naming of names

---
+++ src/main/java/com/codelesscode/presentation/TheNamingOfNames.java
@@ -0,0 +1,23 @@
+package com.codelesscode.presentation;
+
+public class TheNamingOfNames {
+
+    // Do NOT do this
+    int x;
+    int x1;
+    int x2;
+    int x3;
+    int x4;
+    int x5;
+    int x6;
+    int x7;
+    int x8;
+    int x9;
+    int x10;
+
+    public int performTaxCalculation() {
+        // Hmm was it x2 or x3 we needed to divide by?
+        return x1 / x2 * 100;
+    }
+
+}
