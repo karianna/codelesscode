ec8b541120591b19be87deb9d5345a9142b60680
Martijn Verburg <martijnverburg@gmail.com>
2014-08-28 16:55:15 +0100

    Initial Cut of Empty example

--- src/main/java/com/codelesscode/presentation/Empty.java
+++ src/main/java/com/codelesscode/presentation/Empty.java
@@ -11,7 +11,7 @@ public class Empty {
     private final static Logger LOGGER = LoggerFactory.getLogger(Empty.class);
     
     /** Is initialised to null */
-    List<?> dataCollection = null;
+    List<?> dataCollection = null; // new ArrayList<>();
     
     public static void main(String[] args) {
         Empty me = new Empty();
@@ -19,13 +19,7 @@ public class Empty {
     }
     
     private void readFromCollection() {
-        // LOGGER.info("Collection Size is:" + dataCollection.size());
-        
-        initialiseDataCollection();
         LOGGER.info("Collection Size is:" + dataCollection.size());
     }
 
-    private void initialiseDataCollection() {
-        dataCollection = new ArrayList<String>();
-    }
 }
